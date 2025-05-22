4f57d3f2855f87431cadf7c66aaea3a3d02a02bc
Carlos Roberto Arias Arevalo <32964401+arias-spu@users.noreply.github.com>
2025-05-22 10:14:25 -0700

    dynamic_cast

--- main.cpp
+++ main.cpp
@@ -12,11 +12,29 @@ int main() {
 	vector<Person*> people;
 
 	people.push_back(new Person("Jenny", 12));
-	people.push_back(new Employee("Tommy", 30));
+	people.push_back(new Employee("Tommy", 30, 1000));
 	people.push_back(new Person("Johnny", 18));
 	people.push_back(new Employee("Timmy", 23, 1500));
 	people.push_back(new Person("Jimmy", 21));
 
+	for (int i = 0; i < people.size(); i++) {
+		cout << people.at(i)->ToString() << endl;
+
+	}
+	cout << "--------------------------------------" << endl;
+
+	Employee* employee = nullptr;
+	for (int i = 0; i < people.size(); i++) {
+		employee = dynamic_cast<Employee*>(people.at(i));
+		if (employee == nullptr) {
+			cout << people.at(i)->ToString() << " is not an employee" << endl;
+		}else {
+			cout << employee->ToString() << endl;
+			employee->RaiseSalary(0.10);
+		}
+
+	}
+	cout << "--------------------------------------" << endl;
 	for (int i = 0; i < people.size(); i++) {
 		cout << people.at(i)->ToString() << endl;
 
